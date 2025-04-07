ef3e68175abf851f164407eac04e4d5c272c17e0
Nirbhay Sirsikar <108933885+NirbhaySirsikar@users.noreply.github.com>
2025-04-07 17:30:54 +0530

    removed unnecessary comments

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-/// Data class representing the connection details needed to join a LiveKit room
-/// This includes the server URL and auth token
 class ConnectionDetails {
   final String serverUrl;
   final String participantToken;
@@ -18,22 +16,6 @@ class ConnectionDetails {
   }
 }
 
-/// An example service for fetching LiveKit authentication tokens
-///
-/// To use the LiveKit Cloud sandbox (development only)
-/// - Enable your sandbox here https://cloud.livekit.io/projects/p_/sandbox/templates/token-server
-/// - Create .env file with your LIVEKIT_SANDBOX_ID
-///
-/// To use a hardcoded token (development only)
-/// - Generate a token: https://docs.livekit.io/home/cli/cli-setup/#generate-access-token
-/// - Set `hardcodedServerUrl` and `hardcodedToken` below
-///
-/// To use your own server (production applications)
-/// - Add a token endpoint to your server with a LiveKit Server SDK https://docs.livekit.io/home/server/generating-tokens/
-/// - Modify or replace this class as needed to connect to your new token server
-/// - Rejoice in your new production-ready LiveKit application!
-///
-/// See https://docs.livekit.io/home/get-started/authentication for more information
 class TokenService extends ChangeNotifier {
   final String participantId;
   final String companyId;
